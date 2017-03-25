5c5a7a528c2258f79802cd10409f0078775bfca4
namangupta01 <01namangupta@gmail.com>
2017-03-25 19:30:44 +0530

    More Information Added

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ searchButton.addEventListener("click",function(){
 		}
 	});
 function render(data){
-	str="<div><div><li>Temp: " +data.current.temp_c+"</li><li>Last Updated at : "+data.current.last_updated
-	+"</li><li>Place: "+data.location.name+"</li><li>Region: "+data.location.region+"</li></div></div>";
+	str="<div><div><li>Temperature: " +data.current.temp_c+" C </li><li>Last Updated at : "+data.current.last_updated
+	+"</li><li>Place: "+data.location.name+"</li><li>Region: "+data.location.region+"</li><li>Country: "+data.location.country+
+	"</li><li>Condition: "+data.current.condition.text+"</li><li>Wind Speed : "+data.current.wind_mph+"mph/"+
+	data.current.wind_kph+"kph</li><li>Wind Degree: "+data.current.wind_degree+"</li><li>Wind Direction: "+data.current.wind_dir+
+	"</li><li>Precipitation: "+data.current.precip_in+"</li><li>Humidity: "+data.current.humidity+"</li><li>Cloud: "+data.current.cloud+
+	"</li></div></div>";
 	details.innerHTML = str;	
 }
