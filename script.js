9cfca15cb2074be370456c1904e8567ddff40d09
namangupta01 <01namangupta@gmail.com>
2017-03-26 19:40:24 +0530

    minor bug removed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var searchButton=document.getElementById("searchButton");
 var searchBar=document.getElementById("searchBar");
 searchButton.addEventListener("click",function(){
 		var searchValue=searchBar.value;
-		var dataWebsite="https:query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+searchValue+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+		var dataWebsite="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+searchValue+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 		var request = new XMLHttpRequest();
 		request.open('GET' , dataWebsite);
 		request.send();
