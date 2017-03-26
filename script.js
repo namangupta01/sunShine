5bbd8eac0ccdca1517277d055a28e7ce6968a67e
namangupta01 <01namangupta@gmail.com>
2017-03-26 19:23:21 +0530

    api changed from apixu to yahoo weather api

--- script.js
+++ script.js
@@ -2,21 +2,17 @@ var searchButton=document.getElementById("searchButton");
 var searchBar=document.getElementById("searchBar");
 searchButton.addEventListener("click",function(){
 		var searchValue=searchBar.value;
-		var dataWebsite="https://api.apixu.com/v1/current.json?key=27f4205113ea4b4ca94103008172503&q="+searchValue;
+		var dataWebsite="https:query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+searchValue+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 		var request = new XMLHttpRequest();
 		request.open('GET' , dataWebsite);
 		request.send();
+        console.log(request);
 		request.onload = function(){
 			var data = JSON.parse(request.responseText);
 			render(data);
 		}
 	});
 function render(data){
-	str="<div><div><li>Temperature: " +data.current.temp_c+" C </li><li>Last Updated at : "+data.current.last_updated
-	+"</li><li>Place: "+data.location.name+"</li><li>Region: "+data.location.region+"</li><li>Country: "+data.location.country+
-	"</li><li>Condition: "+data.current.condition.text+"</li><li>Wind Speed : "+data.current.wind_mph+"mph/"+
-	data.current.wind_kph+"kph</li><li>Wind Degree: "+data.current.wind_degree+"</li><li>Wind Direction: "+data.current.wind_dir+
-	"</li><li>Precipitation: "+data.current.precip_in+"</li><li>Humidity: "+data.current.humidity+"</li><li>Cloud: "+data.current.cloud+
-	"</li></div></div>";
-	details.innerHTML = str;	
+    str="<div><div><li>Temperature: "+data.query.results.channel.item.condition.temp+" "+data.query.results.channel.units.temperature+"</li><li>Last Updated at: "+data.query.results.channel.lastBuildDate+"</li><li>Location: "+data.query.results.channel.location.city+"</li><li>Country: "+data.query.results.channel.location.country+"</li><li>Region: "+data.query.results.channel.location.region+"</li><li>Wind Speed:"+data.query.results.channel.wind.direction+" "+data.query.results.channel.units.speed+"</li><li>Humidity: "+data.query.results.channel.atmosphere.humidity+"</li><li>Pressure :"+data.query.results.channel.atmosphere.pressure+data.query.results.channel.units.pressure+"</li><li>Visiblity :"+data.query.results.channel.atmosphere.visibility+"</li><li>Sunrise: "+data.query.results.channel.astronomy.sunrise+"</li><li>Sunset: "+data.query.results.channel.astronomy.sunset+"</li><li>Condition: "+data.query.results.channel.item.condition.text+"</li></div></div>"
+    details.innerHTML = str;
 }
